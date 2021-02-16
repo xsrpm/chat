8e0a0052f81590ca71f187cf9acc87bd39e29c20
César Palma <cpalmamorante@gmail.com>
2021-02-16 23:43:56 +0000

    chat funcional en repl.it

--- server.js
+++ server.js
@@ -1,26 +1,21 @@
-
-const http = require('http');
-const fs = require('fs');
 const ws = new require('ws');
+const express = require('express')
+const app = express()
 
 const wss = new ws.Server({noServer: true});
 
-const clients = new Set();
-
-function accept(req, res) {
-
-  if (req.url == '/ws' && req.headers.upgrade &&
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+app.get('/ws', (req, res) => {
+  if(req.headers.upgrade &&
       req.headers.upgrade.toLowerCase() == 'websocket' &&
       // can be Connection: keep-alive, Upgrade
-      req.headers.connection.match(/\bupgrade\b/i)) {
-    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
-  } else if (req.url == '/') { // index.html
-    fs.createReadStream('./index.html').pipe(res);
-  } else { // page not found
-    res.writeHead(404);
-    res.end();
-  }
-}
+      req.headers.connection.match(/\bupgrade\b/i)){
+        wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
+      }
+});
 
 function onSocketConnect(ws) {
   clients.add(ws);
@@ -40,4 +35,30 @@ function onSocketConnect(ws) {
   });
 }
 
-http.createServer(accept).listen(8080);
+// Not found middleware
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'})
+})
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
